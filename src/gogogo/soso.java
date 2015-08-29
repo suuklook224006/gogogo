2c04cd911b2fe326fb74b4b14e797f4ae509254a
小柏 <@PC>
2015-08-29 16:11:42 +0800

    第二次上傳 修改資料

--- src/gogogo/soso.java
+++ src/gogogo/soso.java
@@ -5,6 +5,9 @@ public class soso {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("gogogo");
+		System.out.println("popopo");
+		System.out.println("popopo1");
+		System.out.println("popopo1");
 	}
 
 }
